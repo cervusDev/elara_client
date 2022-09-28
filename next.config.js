24c334ada443a6035a056d20394971ef9feff90a
CervusDev <gustavo.cervera@macroplan.com.br>
2022-09-28 17:49:42 -0300

    chore: general configs

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const withImages = require('next-images')
 
-module.exports = nextConfig
+module.exports = withImages({
+  esModule: true,
+})
